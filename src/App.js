2bd0b914eabe6735553a9f895e633f91e48bc7d7
7rahib <rahibrashid002@gmail.com>
2022-09-22 10:52:40 +0600

    daisyUI installed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
