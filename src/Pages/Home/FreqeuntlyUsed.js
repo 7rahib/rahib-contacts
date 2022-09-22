478ba7c6ed9888339829f2011dfad5d03db8c340
7rahib <rahibrashid002@gmail.com>
2022-09-22 11:34:57 +0600

    contact table created

--- src/Pages/Home/FreqeuntlyUsed.js
+++ src/Pages/Home/FreqeuntlyUsed.js
@@ -2,8 +2,68 @@ import React from 'react';
 
 const FreqeuntlyUsed = () => {
     return (
-        <div>
-            <h3 className='text-3xl font-semibold'>Frequently Used Contacts</h3>
+        <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Job Title & Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox" />
+                            </label>
+                        </th>
+                        <td>
+                            <div className="flex items-center space-x-3">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="font-bold">Naimur Rashid Rahib</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            rahib@gmail.com
+                        </td>
+                        <td>+8801785766545</td>
+                        <td>Student at Leading University, Sylhet</td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox" />
+                            </label>
+                        </th>
+                        <td>
+                            <div className="flex items-center space-x-3">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="font-bold">Naimur Rashid Rahib</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            rahib@gmail.com
+                        </td>
+                        <td>+8801785766545</td>
+                        <td>Student at Leading University, Sylhet</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,6 @@ import { Link, Outlet } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-
             <div className="drawer drawer-mobile lg:p-8">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
@@ -12,12 +11,14 @@ const Home = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                    <ul className="menu p-4 overflow-y-auto w-80 text-base-content">
                         <li><Link to='/'>Contacts</Link></li>
-                        <li><Link to='/frequentlyUsed'>Frequently Contacted</Link></li>
+                        <li><Link to='/frequentlyUsed' className='mt-1'>Frequently Contacted</Link></li>
                     </ul>
                 </div>
             </div>
+            <button title="Add new Contact"
+                class="fixed z-90 bottom-10 right-8 bg-white w-20 h-20 rounded-full border-solid border-2 shadow-xl flex justify-center items-center text-white text-4xl hover:bg-base-100 hover:drop-shadow-xl hover:shadow-2xl"><span className='text-2xl'>➕</span></button>
         </div>
     );
 };
